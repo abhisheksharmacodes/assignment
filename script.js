910d1a71b4fc4162e90a3aafe9d3302dafda424b
Abhishek Sharma <geekysharma31@gmail.com>
2024-10-18 23:00:42 +0530

    enchanced efficiency

--- script.js
+++ script.js
@@ -1,99 +1,82 @@
+let selected = 2;   // indicator of selected option
 
-let selected = 2   // referring to selected option
+// References to dom elements 
 
-// References to dom elements
+const options = document.getElementById('container');
+const option1 = document.getElementById('option1');
+const option2 = document.getElementById('option2');
+const option3 = document.getElementById('option3');
 
-let opt1 = document.getElementById('option1')
-let opt2 = document.getElementById('option2')
-let opt3 = document.getElementById('option3')
+const content1 = options.querySelector('#option1').innerHTML;
+const content2 = options.querySelector('#option2').innerHTML;
+const content3 = options.querySelector('#option3').innerHTML;
 
-let content1 = opt1.innerHTML
-let content2 = opt2.innerHTML
-let content3 = opt3.innerHTML
-
-// Content to be appended when an option is selected
-
-let content = `
-<table>
-    <tr  class="first_child">
-        <td></td>
-        <td>Size</td>
-        <td>Color</td>
+// size and color option to be appended to option when clicked
+const content = `
+  <table>
+    <tr class="first_child">
+      <td></td>
+      <td>Size</td>
+      <td>Color</td>
     </tr>
     <tr>
-        <td class="first_child">#1</td>
-        <td>
-            <select>
-                <option value="S">S</option>
-                <option value="M">M</option>
-                <option value="L">L</option>
-            </select>
-        </td>
-        <td>
-            <select>
-                <option value="Black">Black</option>
-                <option value="White">White</option>
-                <option value="Blue">Blue</option>
-            </select>
-        </td>
+      <td class="first_child">#1</td>
+      <td>
+        <select>
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
+        </select>
+      </td>
+      <td>
+        <select>
+          <option value="Black">Black</option>
+          <option value="White">White</option>
+          <option value="Blue">Blue</option>
+        </select>
+      </td>
     </tr>
     <tr>
-        <td class="first_child">#2</td>
-        <td>
-            <select>
-                <option value="S">S</option>
-                <option value="M">M</option>
-                <option value="L">L</option>
-            </select>
-        </td>
-        <td>
-            <select>
-                <option value="Black">Colour</option>
-                <option value="White">White</option>
-                <option value="Blue">Blue</option>
-            </select>
-        </td>
+      <td class="first_child">#2</td>
+      <td>
+        <select>
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
+        </select>
+      </td>
+      <td>
+        <select>
+          <option value="Black">Colour</option> <option value="White">White</option>
+          <option value="Blue">Blue</option>
+        </select>
+      </td>
     </tr>
-</table>
-`
+  </table>
+`;
 
-// Handling clicks on options for appending content and checking radioboxes respectively
+option2.innerHTML = content2 + content   // initializing the size and color options
 
-opt2.innerHTML = content2 + content
-document.getElementById('unit_2').setAttribute('checked', 'checked')
+options.addEventListener('click', (event) => {      // Handling clicking on boxes to append size and color options and checking respective radio box 
+    const clickedOption = event.target.closest('.options');     // selecting the closest element having option class closest to the clicked element
+    if (!clickedOption) return;
 
-opt1.onclick = () => {
-    if (selected !== 1) {
-        undo()
-        opt1.innerHTML = content1 + content
-        document.getElementById('unit_1').setAttribute('checked', 'checked')
-        selected = 1
+    if (clickedOption.id !== `option${selected}`) {
+        undo();
+        // appending the size and color options and selecting radio box
+        clickedOption.innerHTML = (clickedOption.id === 'option1' ? content1 : clickedOption.id === 'option2' ? content2 : content3) + content;
+        document.getElementById('unit_1').checked = clickedOption.id == 'option1'
+        document.getElementById('unit_2').checked = clickedOption.id == 'option2'
+        document.getElementById('unit_3').checked = clickedOption.id == 'option3'
+        selected = parseInt(clickedOption.id.slice(6));
     }
-}
-opt2.onclick = () => {
-    if (selected !== 2) {
-        undo()
-        opt2.innerHTML = content2 + content
-        document.getElementById('unit_2').setAttribute('checked', 'checked')
-        selected = 2
-    }
-}
-opt3.onclick = () => {
-    if (selected !== 3) {
-        undo()
-        opt3.innerHTML = content3 + content
-        document.getElementById('unit_3').setAttribute('checked', 'checked')
-        selected = 3
-    }
-}
+});
 
-// Function for undoing appends and radiobox checks
-
-let undo = () => {
-    opt1.innerHTML = content1
-    opt2.innerHTML = content2
-    opt3.innerHTML = content3
-    document.getElementById('unit_1').removeAttribute('checked')
-    document.getElementById('unit_2').removeAttribute('checked')
-    document.getElementById('unit_3').removeAttribute('checked')
+function undo() {   // for undoing the changes such as content appendation and radiobox selection
+    option1.innerHTML = content1;
+    option2.innerHTML = content2;
+    option3.innerHTML = content3;
+    document.getElementById('unit_1').checked = false;
+    document.getElementById('unit_2').checked = false;
+    document.getElementById('unit_3').checked = false;
 }
